f310ada886056513f485ef16e15f4566c2e49bbe
JalenNeck <132936602+JalenNeck@users.noreply.github.com>
2024-10-30 10:01:26 -0400

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -103,7 +103,7 @@ class Transaction :
 
    # getTType returns the transaction type of the transaction.
    # @return: The transaction type of the transaction
-   def getTType(self) :
+   def _getTType(self) :
       return self._tType
    
    # Prints all of the transaction instance variables.
